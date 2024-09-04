6d87abfb2fb480b0b1f8cc87756c2568984fdcfb
jonnathandev <jonnathan.perez@mercadolibre.com.co>
2024-09-03 23:03:45 -0500

    feat: update form login

--- clean_stock/src/App.tsx
+++ clean_stock/src/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+        <div className='seccion-login'>
+          <div className='card card-informative'>
+            <img src="https://www.sitew.com/images/blog/articles/guides_users_es/seo_simple/ejemplos_logos/starbucks_es.png" alt="CleanStock logo" className='logo-clean-stock'/>
+            <p>
+              CleanStock es una aplicación innovadora diseñada para optimizar la gestión de inventarios en las organizaciones. Nuestra app facilita el análisis y la administración eficiente de productos, proporcionando a nuestros clientes una visión integral y estratégica de sus inventarios. Inicialmente, CleanStock estará disponible como una aplicación web que require conexión a internet. Sin embargo, nuestro objectivo a largo plazo es desarrollar una version movil que permita a los usuarios gestionar sus inventarios de manera efectiva, incluso sin conexión a internet.
+            </p>
+          </div>
+          <div className='card card-form'>
+            <h1>Card 1</h1>
+            <input type="text" name='userName' placeholder='User name'/>
+            <input type="text" name='password' placeholder='Password'/>
+            <button>Login</button>
+          </div>
+        </div>
       </div>
-      <h1>Clean Stock</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
